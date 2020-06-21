0d01efbe7d4a8e3f0aeb5c2e0b0cfd4de2ea8cb7
DarkCaster <dark.caster@outlook.com>
2020-06-21 04:56:10 +0300

    dnsdist-lite package updated, added example config-script

---
+++ dnsdist-lite/files/dnsdist.cfg.lua
@@ -0,0 +1,84 @@
+--[[
+
+sample dnsdist configuration-script for generating rules
+for matching dns-queries' qnames against regexps (using built-in or re2 engine)
+and also for optional matching against list of qtypes
+matched queries will be forwarded to configured pools of dns-servers
+for all unmatched queries NXDOMAIN response will be returned
+
+please, place all regexp-definitions and other config (backend dns-servers and pool-configs, acls, etc...)
+into dnsdist.defs.lua config file: it will be executed first
+also, you may add some other configs and rules into dnsdist.post.lua
+
+WARNING: this is highly experimental stuff, and may not work as you expected
+consider not to use this script in production env, you have been warned
+
+]]--
+
+-- table names with matches
+rulesDefTableName="definitions"
+
+-- load "definitions" table from dnsdist.defs.lua config file, and configure other stuff before generating rules
+dofile("/etc/dnsdist.defs.lua")
+
+assert(rulesDefTableName~=nil and rulesDefTableName~="rulesTable", "rulesDefTableName is invalid")
+
+rulesTable=load("return " .. rulesDefTableName)()
+
+assert(type(rulesTable)=="table","global table with name '" .. rulesDefTableName .. "' is not found")
+
+-- parse definitions table and generate rules. this code may be non optimal
+for ruleIdx,ruleDef in ipairs(rulesTable) do
+	assert(type(ruleDef)=="table","rule definition at position #"..ruleIdx.." must be a table")
+	assert((type(ruleDef.rx)=="nil" and type(ruleDef.re2)=="string") or (type(ruleDef.rx)=="string" and type(ruleDef.re2)=="nil"),"rule definition at position #"..ruleIdx.." must contain 'rx' of 're2' regexp definition")
+
+	-- create regex rule selector, using either standard regex or re2 engines
+	local regexRule
+	local regexDebug -- for printing verbose messages about added rules
+	if(type(ruleDef.rx)=="string") then
+		regexDebug="regex rule '"..ruleDef.rx.."'"
+		regexRule=RegexRule(ruleDef.rx)
+	else
+		regexDebug="re2 rule: "..ruleDef.re2
+		regexRule=RE2Rule(ruleDef.re2)
+	end
+
+	assert(type(ruleDef.p)=="string","rule definition at position #"..ruleIdx.." must contain 'p' pool definition")
+	assert((type(ruleDef.t)=="nil" and type(ruleDef.nt)=="table") or (type(ruleDef.t)=="table" and type(ruleDef.nt)=="nil") or (type(ruleDef.t)=="nil" and type(ruleDef.nt)=="nil"),"rule definition at position #"..ruleIdx.." must contain qtype definition as either 't' or 'nt' table, or neither")
+
+	if (type(ruleDef.t)=="nil" and type(ruleDef.nt)=="nil") then
+		-- add simple regexp rule without matching queries against qtypes
+		print("Adding "..regexDebug.." to pool "..ruleDef.p)
+		addAction(regexRule,PoolAction(ruleDef.p))
+	else
+		-- add action with qname-regexp and qtypes matching
+		local qtDebug=" matching QTypes: " -- for printing verbose messages about added rules
+		local qt -- temporary value storing table with qtypes
+		local qtrules={} --table with QTypeRule rules
+		local qtrules_added=false --for checking against empty qtypes table
+		if (type(ruleDef.t)=="table") then qt=ruleDef.t else qtDebug=" NOT "..qtDebug; qt=ruleDef.nt end
+
+		-- iterate over 't' or 'nt' table, create QTypeRules and save it to qtrules table
+		for qi,q in ipairs(qt) do
+			assert(type(q)=="number","rule definition at position #"..ruleIdx.." contains invalid DNSQtype at position #"..qi)
+			qtDebug=qtDebug..q..","
+			table.insert(qtrules,QTypeRule(q))
+			qtrules_added=true
+		end
+		assert(qtrules_added==true,"rule definition at position #"..ruleIdx.." do not contain non-empty table with valid DNSQtype entries")
+
+		-- add regexp rule with matching queries against list of qtypes
+		print("Adding "..regexDebug..";"..qtDebug.." to pool "..ruleDef.p)
+		if (type(ruleDef.t)=="table") then
+			addAction(AndRule({regexRule,OrRule(qtrules)}),PoolAction(ruleDef.p))
+		else
+			addAction(AndRule({regexRule,NotRule(OrRule(qtrules))}),PoolAction(ruleDef.p))
+		end
+	end
+end
+
+-- run dnsdist.post.lua config file before adding last rule for any dns-query not handled with rules generated before
+dofile("/etc/dnsdist.post.lua")
+
+-- add action that will generate negative response for all other non-matched queries
+addAction(AllRule(),SetNegativeAndSOAAction(true,"dnsdist",1800,"dnsdist","dnsdist",0,86400,7200,3600000,1800))
---
+++ dnsdist-lite/files/dnsdist.defs.lua
@@ -0,0 +1,27 @@
+-- TODO: not finished example
+
+-- listen on 127.0.0.1:777, allow access from 
+setLocal("127.0.0.1:7777")
+setACL({"127.0.0.1/8","192.168.1.0/24"})
+
+-- define pools of DNS servers
+pools={
+	lc="local",
+	bp="bypass",
+}
+
+-- define dns servers
+newServer({address="127.0.0.53:53", pool={pools.lc},name="dnsmasq",setCD=true}) -- TODO: checkfunction, check retries, etc
+newServer({address="8.8.8.8:53", pool=pools.bp,name="google"})
+
+--[[ matches table example, rules will be generated from top to bottom 
+definitions={
+	{rx="regex",p=pools.lc,t={DNSQtype,DNSQtype,...}} -- match request name against regular regexps, and provided dnsqtypes
+	{re2="regex",p=pools.bp,nt={DNSQtype,DNSQtype,...}} -- match request name against regexps using re2 engine, and dnsqtype mus be NOT among provided list
+}
+--]]
+
+-- table with definitions for regexp matching-rules
+definitions={
+	{rx="google.com",p=pools.lc},
+}
