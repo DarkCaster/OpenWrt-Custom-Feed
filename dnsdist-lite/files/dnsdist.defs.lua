e9f78ac7979120bc224b8f264e03617087696f89
DarkCaster <dark.caster@outlook.com>
2020-06-24 02:53:01 +0300

    dnsdist-lite package updated

--- dnsdist-lite/files/dnsdist.defs.lua
+++ dnsdist-lite/files/dnsdist.defs.lua
@@ -1,33 +1,35 @@
--- TODO: not finished example
-
--- listen on 127.0.0.1:777, allow access from 
-setLocal("127.0.0.1:5300")
-addLocal('192.168.1.1:5300')
-setACL({"127.0.0.1/8","192.168.1.0/24"})
+-- just an example, not for production use
+setLocal("127.0.0.1:53")
+addLocal("192.168.1.1:53")
+controlSocket("127.0.0.1:5199")
+webserver("192.168.1.1:5198", "password", "magickey", {}, "192.168.1.0/24, 192.168.5.0/24")
+setACL({"127.0.0.1/8","192.168.1.0/24","192.168.5.0/24"})
 
 -- define pools of DNS servers
 pools={
 	lc="local",
 	ext="external",
+	isp="isp",
 }
 
 setVerboseHealthChecks(true)
 
 -- define dns servers
-newServer({address="1.1.1.1", pool={pools.ext}, name="cloudflare1", mustResolve=false, checkTimeout=2500, checkInterval=20, maxCheckFailures=2, qps=10, order=10})
-newServer({address="1.0.0.1", pool={pools.ext}, name="cloudflare2", mustResolve=false, checkTimeout=2500, checkInterval=21, maxCheckFailures=2, qps=20, order=10})
-newServer({address="8.8.8.8", pool={pools.ext}, name="google1", mustResolve=false, checkTimeout=2500, checkInterval=40, maxCheckFailures=2, qps=25, order=20})
-newServer({address="8.8.4.4", pool={pools.ext}, name="google2", mustResolve=false, checkTimeout=2500, checkInterval=41, maxCheckFailures=2, qps=25, order=20})
-newServer({address="208.67.222.222", pool={pools.ext}, name="opendns1", mustResolve=false, checkTimeout=5000, checkInterval=60, maxCheckFailures=2, qps=5, order=30})
-newServer({address="208.67.220.220", pool={pools.ext}, name="opendns2", mustResolve=false, checkTimeout=5000, checkInterval=61, maxCheckFailures=2, qps=5, order=30})
+newServer({address="1.1.1.1", pool={pools.ext}, name="cloudflare1", mustResolve=false, checkTimeout=2500, checkInterval=20, maxCheckFailures=2, qps=4, order=10})
+newServer({address="1.0.0.1", pool={pools.ext}, name="cloudflare2", mustResolve=false, checkTimeout=2500, checkInterval=21, maxCheckFailures=2, qps=50, order=10})
+newServer({address="8.8.8.8", pool={pools.ext}, name="google1", mustResolve=false, checkTimeout=2500, checkInterval=40, maxCheckFailures=2, qps=3, order=20})
+newServer({address="8.8.4.4", pool={pools.ext}, name="google2", mustResolve=false, checkTimeout=2500, checkInterval=41, maxCheckFailures=2, qps=3, order=20})
+newServer({address="208.67.222.222", pool={pools.ext}, name="opendns1", mustResolve=false, checkTimeout=5000, checkInterval=60, maxCheckFailures=2, qps=2, order=30})
+newServer({address="208.67.220.220", pool={pools.ext}, name="opendns2", mustResolve=false, checkTimeout=5000, checkInterval=61, maxCheckFailures=2, qps=2, order=30})
 
-newServer({address="127.0.0.1", pool={pools.lc}, name="dnsmasq", mustResolve=true, checkTimeout=2500, checkInterval=30, maxCheckFailures=2, qps=100, order=10
-})
-newServer({address="8.8.8.8", pool={pools.lc}, name="fallback", mustResolve=false, checkTimeout=5000, checkInterval=62, maxCheckFailures=2, qps=5, order=10
-})
+newServer({address="127.0.0.1:5353", pool={pools.lc}, name="local", checkName="openwrt.", mustResolve=true, checkTimeout=5000, checkInterval=5, maxCheckFailures=2, qps=100, order=10})
+
+newServer({address="127.0.0.1:5353", pool={pools.isp}, name="isp", mustResolve=true, checkTimeout=5000, checkInterval=62, maxCheckFailures=2, qps=10000, order=10})
+newServer({address="8.8.8.8", pool={pools.isp}, name="isp", mustResolve=false, checkTimeout=5000, checkInterval=63, maxCheckFailures=2, qps=50, order=20})
 
 setPoolServerPolicy(firstAvailable, pools.lc)
 setPoolServerPolicy(firstAvailable, pools.ext)
+setPoolServerPolicy(firstAvailable, pools.isp)
 
 setCacheCleaningPercentage(50)
 setStaleCacheEntriesTTL(300)
@@ -36,10 +38,12 @@ extPC=newPacketCache(250,{temporaryFailureTTL=5, staleTTL=30})
 locPC=newPacketCache(250,{temporaryFailureTTL=5, staleTTL=30})
 
 getPool(pools.ext):setCache(extPC)
-getPool(pools.lc):setCache(locPC)
+getPool(pools.isp):setCache(locPC)
 
--- table with definitions for regexp matching-rules
+-- example definitions for regexp matching-rules
+-- rules will be created by logic at dnsdist.cfg.lua script
 definitions={
+--[[
 	-- queries to google.com. will be processed with regular regexp engine (rx field name, use re2 field name for use libre2 engine)
 	{rx="google.com",d=-1,t={DNSQType.MX, DNSQType.NS}}, -- drop queries with qtypes DNSQType.NS or DNSQType.MX completely
 	{rx="google.com",d=DNSRCode.NOERROR,t={DNSQType.A,DNSQType.ANY}}, -- answer instantly with NOERROR response for queries of type A (ipv4) or ANY
@@ -47,4 +51,19 @@ definitions={
 	{rx="google.com",p=pools.lc}, -- any other queries to google.com will be forwarded to dns servers from "local" pool
 	-- queries to example.com
 	{rx="example.com",p=pools.lc,nt={DNSQType.A,DNSQType.ANY}} -- all queries with qtypes NOT equal to DNSQType.A or DNSQType.ANY will be forwarded to "local" pool
+]]--
+	-- local subnet
+	{re2="(?i)^.*\\.lan$",p=pools.lc},
+	{re2="(?i)^[^\\.]*$",p=pools.lc},
+	-- overrides
+	{re2="(?i)^(.+\\.|)ipv6-test\\.com$",p=pools.ext},
+	{re2="(?i)^(.+\\.|)mozilla\\.[a-z]*$",p=pools.ext},
+	{re2="(?i)^(.+\\.|)firefox\\.[a-z]*$",p=pools.ext},
+	{re2="(?i)^(.+\\.|)mozaws\\.net*$",p=pools.ext},
+	-- disable ipv6 answers for all other requests
+	{re2=".*",d=DNSRCode.NOERROR,t={DNSQType.AAAA}},
+	-- com zone
+	{re2="(?i)^.*.com$",p=pools.isp},
+	-- all other addresses
+	{re2=".*",p=pools.ext},
 }
